2658770959f1cf7367aa105f16802afdda11ff45
ic3top <potatai123@gmail.com>
2021-04-05 17:34:59 +0300

    vue-config fix

--- budget-app/vue.config.js
+++ budget-app/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/Vue-path/budget-app/dist/'
+        ? '/budget-app/dist/'
         : '/'
-}
+};
--- movies/vue.config.js
+++ movies/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue-path/movies/dist/'
+    ? '/movies/dist/'
     : '/'
 };
